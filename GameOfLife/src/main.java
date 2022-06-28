2c632bf14fd0a476374ec4ad151a28232880a6c2
Ryan Han <77809236+ryanhan1201@users.noreply.github.com>
2022-06-27 18:32:21 -0700

    fixed bugs with countPerim

--- GameOfLife/src/main.java
+++ GameOfLife/src/main.java
@@ -1,26 +1,45 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+
+import java.io.PrintWriter;
+
 import javax.swing.JFrame;
 class World {
 	int[][] grid;
-	static int[][] prevGen;
+	int[][] prevGen;
 	//Initiallizing a Grid of 50 X 50 where we have 9 middle cells be alive
 	/*
 	 * Representing the grid in terms of ints where
 	 * 1 = alive
 	 * 0 = dead
 	 */
-	public final static int GRID_LENGTH = 50;
-	public final static int GRID_WIDTH = 50;
+	// public final static int GRID_LENGTH = 50;
+	// public final static int GRID_WIDTH = 50;
+	public final static int GRID_LENGTH = 9;
+	public final static int GRID_WIDTH = 9;
 	public World(){
-		grid = new int[50][50];
-		grid[24][24] = 1;
-		grid[24][25] = 1;
-		grid[24][23] = 1;
-		grid[25][24] = 1;
-		grid[23][24] = 1;
-		grid[23][23] = 1;
-		grid[23][25] = 1;
-		grid[25][25] = 1;
-		grid[25][23] = 1;
+		grid = new int[9][9];
+		grid[4][4] = 1;
+		grid[4][5] = 1;
+		grid[4][3] = 1;
+		grid[5][4] = 1;
+		grid[5][5] = 1;
+		grid[5][3] = 1;
+		grid[3][4] = 1;
+		grid[3][5] = 1;
+		grid[3][3] = 1;
+		// grid = new int[GRID_LENGTH][GRID_WIDTH];
+		// grid[24][24] = 1;
+		// grid[24][25] = 1;
+		// grid[24][23] = 1;
+		// grid[25][24] = 1;
+		// grid[23][24] = 1;
+		// grid[23][23] = 1;
+		// grid[23][25] = 1;
+		// grid[25][25] = 1;
+		// grid[25][23] = 1;
+	
 	}
 	public void setPrevGen(int[][] grid) {
 		prevGen = new int[GRID_LENGTH][GRID_WIDTH];
@@ -33,13 +52,13 @@ class World {
 	}
 	public int countPerim(int i, int j) {
 		int count = 0;
-		for(int row = i - 1; i < i + 2; i++) {
-			for(int col = j - 1; j < j + 2; j++) {
+		for(int row = i - 1; row < i + 2; row++) {
+			for(int col = j - 1; col < j + 2; col++) {
 				if(row < 0) continue;
-				if(col < 0) continue;
-				if(row > GRID_LENGTH) continue;
-				if(col > GRID_LENGTH) continue;
-				if(row == i && col == j) continue;
+				else if(col < 0) continue;
+				else if(row >= GRID_LENGTH) continue;
+				else if(col >= GRID_LENGTH) continue;
+				else if(row == i && col == j) continue;
 				if(isAlive(row, col)) count++;
 			}
 		}
@@ -52,17 +71,18 @@ class World {
 public class main {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("GraphicOutput.txt")));
 		/*implement when done with Console outputs */
 		//setup Graphic Output
 		// GraphicOutput m = new GraphicOutput();
 		// JFrame f = new JFrame();
 		// f.add(m);
 		World w = new World();
-		printGrid(w.grid);
-		
+		printGrid(w.grid, out);
 		int gen = 0;
 		while(gen != 1000) { //run for 1000 generations
+			System.out.println("Inside While Loop");
 			w.setPrevGen(w.grid);
 
 			//Use nested for loop to scan for each cell
@@ -70,6 +90,7 @@ public class main {
 			// int count_dead = 0;
 			for(int i = 0; i < World.GRID_LENGTH; i++) {
 				for(int j = 0; j < World.GRID_WIDTH; j++) {
+					System.out.println("In nested for loop");
 					// if(w.isAlive(i,j)) {
 					// 	// count_alive++;
 					// }
@@ -77,7 +98,7 @@ public class main {
 					// 	// count_dead++;
 					// }
 					int count = w.countPerim(i,j);
-
+					System.out.println("Count at " + i + " : " + j + " = " + count);
 					//per cell need to check on the 4 conditions 
 					if(w.isAlive(i, j)) {
 						//check number of alive cells around it
@@ -97,21 +118,26 @@ public class main {
 						}
 					}
 				}
+				System.out.println("Stuck here");
 			}
+			printGrid(w.grid, out);
 			gen++;
 			// System.out.println(count_alive + " : " + count_dead);
 		} 
 		System.out.println("Done");
 
-
+		out.close();
 	}
-	public static void printGrid(int[][] grid) {
+	public static void printGrid(int[][] grid, PrintWriter out) {
 		for(int i = 0; i < World.GRID_LENGTH; i++) {
 			for(int j = 0; j < World.GRID_WIDTH; j++) {
 				System.out.print(grid[i][j] + " ");
+				out.print(grid[i][j] + " ");
 			}
+			out.println();
 			System.out.println();
 		}
+		out.println("");
 		System.out.println("-------------------------------------------------");
 	}
 }
