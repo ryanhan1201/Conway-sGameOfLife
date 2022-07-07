63178c1788379cbe59caee9365a50116c6d0ac10
ryanhan1201 <ryanhan1201@gmail.com>
2022-07-06 19:17:17 -0700

    Finished implementing Tile stuff from Berkeley cs61b class

--- GameOfLife/src/TileEngine/TETile.java
+++ GameOfLife/src/TileEngine/TETile.java
@@ -5,4 +5,32 @@ import edu.princeton.cs.introcs.StdDraw;
 
 
 public class TETile {
+    private final char character;
+    private final Color textColor;
+    private final Color bgColor;
+
+    public TETIle(char character, Color textColor, Color backgroundColor){
+        this.character = character;
+        this.textColor = textColor;
+        this.backgroundColor = backgroundColor;
+    }
+
+    public void draw(double x, double y) {
+        if (filepath != null) {
+            try {
+                StdDraw.picture(x + 0.5, y + 0.5, filepath);
+                return;
+            } catch (IllegalArgumentException e) {
+                // Exception happens because the file can't be found. In this case, fail silently
+                // and just use the character and background color for the tile.
+            }
+        }
+
+        StdDraw.setPenColor(backgroundColor);
+        StdDraw.filledSquare(x + 0.5, y + 0.5, 0.5);
+        StdDraw.setPenColor(textColor);
+        StdDraw.text(x + 0.5, y + 0.5, Character.toString(character()));
+    }
+
+
 }
---
+++ GameOfLife/src/TileEngine/TileSet.java
@@ -0,0 +1,9 @@
+package TileEngine;
+
+import java.awt.Color;
+
+public class TileSet{
+    public static final TETile ALIVE = new TETile('O', Color.GREEN, Color.BLACK);
+    public static final TETIle DEAD = new TETile('X', Color.RED, Color.BLACK);
+
+}
